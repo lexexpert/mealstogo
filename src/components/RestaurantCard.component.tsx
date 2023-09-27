032e80044bd0da2c1335eb1d6a0f0a553da077f2
ivan.reshetar <ivan.reshetar@snaptravel.com>
2023-09-27 08:38:31 -0400

    Create rating icons

--- src/components/RestaurantCard.component.tsx
+++ src/components/RestaurantCard.component.tsx
@@ -3,19 +3,21 @@ import { Card, Text } from "react-native-paper";
 import styled from "styled-components/native";
 import { useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular, Lato_700Bold } from "@expo-google-fonts/lato";
+import { SvgXml } from "react-native-svg";
+import star from "../../assets/star";
 
 const RestaurantCardStyled = styled(Card)`
-  background-color: ${(props) => props.theme.colors?.bg?.primary};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
-  background-color: ${(props) => props.theme.colors?.bg?.primary};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const Title = styled(Text)`
-  color: ${(props) => props.theme.colors?.ui?.primary};
+  color: ${(props) => props.theme.colors.ui.primary};
   font-size: ${(props) => props.theme.fontSizes.title};
   padding-top: ${(props) => props.theme.space[3]};
   padding-bottom: ${(props) => props.theme.space[2]};
@@ -25,6 +27,12 @@ const Title = styled(Text)`
 const Address = styled(Text)`
   font-size: ${(props) => props.theme.fontSizes.body};
   font-family: ${(props) => props.theme.fonts.body};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
 type RestaurantCardProps = {
@@ -53,6 +61,8 @@ export const RestaurantCard = ({ restaurant }: RestaurantCardProps) => {
     return null;
   }
 
+  const ratingArray = Array.from(new Array(Math.floor(restaurant.rating)));
+
   const {
     name = "",
     icon = "",
@@ -67,6 +77,11 @@ export const RestaurantCard = ({ restaurant }: RestaurantCardProps) => {
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Card.Content>
         <Title>{name}</Title>
+        <Rating>
+          {ratingArray.map(() => (
+            <SvgXml xml={star} width={20} height={20} />
+          ))}
+        </Rating>
         <Address>{address}</Address>
       </Card.Content>
     </RestaurantCardStyled>
